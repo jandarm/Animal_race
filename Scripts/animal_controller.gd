e113ebbd0d03dbf4c43dd70c57fefeae18289ad6
Daniel <p0-r2@mail.ru>
2022-04-27 13:05:50 +0300

    Базовый класс животного принимает любую текстуру
    + попытка генерации кнопок
    Общий принцип построения текстур животных

--- Scripts/animal_controller.gd
+++ Scripts/animal_controller.gd
@@ -1,26 +1,31 @@
 extends Node
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var movement_body
 var animation_player
 # Called when the node enters the scene tree for the first time.
+var target = 0
+
 func _ready():
 	movement_body = get_node("Animal_sprite")
 	animation_player = get_node("AnimationPlayer")
 	pass # Replace with function body.
 
 func _process(delta):
+	if movement_body.position.x < target:
+		movement_body.position.x += 5
+	else:
+		stop()
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 func move():
 	animation_player.play("Run")
-	movement_body.position.x += 50
-	stop()
+	movement_body.position.x = target
+	target += 100
 
 func stop():
 	animation_player.play("Idle")
--- Scripts/race_generator.gd
+++ Scripts/race_generator.gd
@@ -9,13 +9,14 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var horse = get_node("Horse")
-	var button1 = Button.new()
 	
-	add_child(button1)
-	button1.set_position(Vector2(100,500))
-	button1.set_size(Vector2(150,120))
-	
-	button1.connect("pressed", horse, "move")
+	for i in range(3):
+		var button1 = Button.new()
+		add_child(button1)
+		button1.set_position(Vector2(i*400,500))
+		button1.set_size(Vector2(150,120))
+		
+		button1.connect("pressed", horse, "move")
 	pass # Replace with function body.
 
 
