afca22004620b8b0f181a1f6887c97fa23cdcd78
Daniel <p0-r2@mail.ru>
2022-05-20 14:15:47 +0300

    Карточки в настройках

--- Scripts/game_settings.gd
+++ Scripts/game_settings.gd
@@ -4,13 +4,19 @@ var blinds
 
 func _ready():
 	blinds = $GridCurtain.get_children()
-	blinds[0].hide()
-	blinds[1].hide()
-	pass
+	open_blinds($TeamNumberBtn.get_selected_id())
 
 func _on_TeamNumberBtn_item_selected(index):
-	blinds[index].hide()
 	GameManager.team_count = $TeamNumberBtn.get_item_text(index)
+	open_blinds(index+2)
 
 func _on_SpinBox_value_changed(value):
 	GameManager.steps_to_win = value
+
+func open_blinds(amount):
+	for element in blinds:
+		if amount > 0:
+			element.hide()
+		else:
+			element.show()
+		amount -= 1
